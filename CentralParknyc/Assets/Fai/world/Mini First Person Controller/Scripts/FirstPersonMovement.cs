682a70659a0c535d02120966e4c74efd044e4b6d
Fai <alkhf824@newschool.edu>
2026-05-12 11:16:23 -0400

    d

--- CentralParknyc/Assets/Fai/world/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ CentralParknyc/Assets/Fai/world/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -3,42 +3,79 @@ using UnityEngine;
 
 public class FirstPersonMovement : MonoBehaviour
 {
-    public float speed = 5;
+    [Header("Movement")]
+    public float speed = 5f;
 
     [Header("Running")]
     public bool canRun = true;
     public bool IsRunning { get; private set; }
-    public float runSpeed = 9;
+    public float runSpeed = 9f;
     public KeyCode runningKey = KeyCode.LeftShift;
 
+    [Header("Drunk Effect")]
+    public bool drunkMovement = true;
+
+    // قوة الترنح
+    public float swayAmount = 2f;
+
+    // سرعة الترنح
+    public float swaySpeed = 4f;
+
+    // اهتزاز زيادة
+    public float randomShake = 0.4f;
+
     Rigidbody rigidbody;
-    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
-    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
 
-
+    public List<System.Func<float>> speedOverrides =
+        new List<System.Func<float>>();
 
     void Awake()
     {
-        // Get the rigidbody on this.
         rigidbody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        // Update IsRunning from input.
+        // Running
         IsRunning = canRun && Input.GetKey(runningKey);
 
-        // Get targetMovingSpeed.
+        // Speed
         float targetMovingSpeed = IsRunning ? runSpeed : speed;
+
         if (speedOverrides.Count > 0)
         {
-            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
+            targetMovingSpeed =
+                speedOverrides[speedOverrides.Count - 1]();
         }
 
-        // Get targetVelocity from input.
-        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+        // Input
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
 
-        // Apply movement.
-        rigidbody.linearVelocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.linearVelocity.y, targetVelocity.y);
+        // DRUNK SWAY
+        if (drunkMovement)
+        {
+            float sway =
+                Mathf.Sin(Time.time * swaySpeed) * swayAmount;
+
+            float shake =
+                Random.Range(-randomShake, randomShake);
+
+            
+            moveX += sway + shake;
+
+            moveZ += Mathf.Cos(Time.time * 2f) * 0.3f;
+        }
+
+        // Apply movement
+        Vector3 move =
+            transform.rotation *
+            new Vector3(
+                moveX * targetMovingSpeed,
+                rigidbody.linearVelocity.y,
+                moveZ * targetMovingSpeed
+            );
+
+        rigidbody.linearVelocity = move;
     }
 }
